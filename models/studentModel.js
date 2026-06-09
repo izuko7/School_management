fef6db15761a57cf059a4eaedc2ec1995536b5ef
izuko7 <izuko7mha@gmail.com>
2026-06-09 21:11:15 +0000

    création des différents modèles

---
+++ models/studentModel.js
@@ -0,0 +1,13 @@
+// Création de la classe étudiant
+
+class Student {
+    constructor(matricule, nom, prenom, age, classe){
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.classe = classe;
+    }
+}
+
+export default Student;
---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+// Création de la classe utilisateur
+
+class Users{
+    constructor(name, role, motdepasse) {
+        this.name = name;
+        this.role = role;
+        this.motdepasse = motdepasse;
+    }
+}
+
+export default Users;
