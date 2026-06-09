f0bfca0c5b51a03df02a2af60331881a4de1eb6e
izuko7 <izuko7mha@gmail.com>
2026-06-09 22:08:54 +0000

    création des différents modèles

---
+++ models/absenceModel.js
@@ -0,0 +1,11 @@
+// Création de la classe Absence
+
+class Absence {
+    constructor(student_id, date, status) {
+        this.student_id = student_id;
+        this.date = date;
+        this.status = status;
+    }
+}
+
+export default Absence;
