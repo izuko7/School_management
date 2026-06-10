27e44d19c6c635ad175e6a7d87dae8123aa41cf0
izuko7 <izuko7mha@gmail.com>
2026-06-10 11:21:12 +0000

    mise à jour du fichier de connexion

--- config/connexion.js
+++ config/connexion.js
@@ -1,27 +1,24 @@
-import { question, fermerInterface } from "./interface.js";
+import { question } from "./interface.js";
 import { getAllUsers } from "../services/userService.js";
-
-// Utilisateur connecté
 
 let userConnecter = null;
 
-// connexion à l'application
 const seConnecter = async () => {
     console.log("\n≈≈≈ CONNEXION ≈≈≈");
     const nom = await question("Nom : ");
     const motdepasse = await question("Mot de passe : ");
- 
+
     const users = getAllUsers();
     const user = users.find(u => u.name === nom && u.motdepasse === motdepasse);
- 
+
     if (!user) {
-        console.log("❌ Utilisateur introuvable. vérifiez votre nom et votre mot de passe.");
+        console.log("❌ Utilisateur introuvable. Vérifiez votre nom et mot de passe.");
         return false;
     }
- 
+
     userConnecter = user;
     console.log(`✅ Bienvenue ${userConnecter.name} (${userConnecter.role}) !`);
     return true;
-}
+};
 
-export { seConnecter, userConnecter }
+export { seConnecter, userConnecter };
