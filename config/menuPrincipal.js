36575d4b16f807ec3e10db53aa4fb54881e5d944
izuko7 <izuko7mha@gmail.com>
2026-06-22 15:54:44 +0000

    ajout des logs au niveau de la connexion et du Menuprincipal

--- config/menuPrincipal.js
+++ config/menuPrincipal.js
@@ -3,7 +3,7 @@ import { seConnecter, session } from "./connexion.js";
 import { menuAdmin } from "./menus/menuAdmin.js";
 import { menuTeacher } from "./menus/menuTeacher.js";
 import { menuStudent } from "./menus/menuStudent.js";
-import { logInfo, logError } from "../utils/logger.js";
+import { logInfo, logWarn, logError, logSucces } from "../utils/logger.js";
 
 // Afficher le message de bienvenue
 const menuPrincipal = async () => {
@@ -23,15 +23,19 @@ const menuPrincipal = async () => {
 
         switch (choix) {
             case "1": {
+                logInfo(`Tentative de connexion`);
                 const connecte = await seConnecter();
                 if (connecte) {
+                    logSucces(`Connexion réussie — utilisateur: ${session.userConnecter.nom}, rôle: ${session.userConnecter.role}`);
                     if (session.userConnecter.role === "admin") await menuAdmin();
                     else if (session.userConnecter.role === "teacher") await menuTeacher();
                     else if (session.userConnecter.role === "student") await menuStudent();
                     else {
                         console.log(" Rôle inconnu.");
-                        logError(`Role inconnu detecte : ${session.userConnecter.role}`);
+                        logError(`Rôle inconnu détecté : ${session.userConnecter.role}`);
                     }
+                } else {
+                    logWarn(`Échec de connexion`);
                 }
                 break;
             }
@@ -42,6 +46,7 @@ const menuPrincipal = async () => {
                 fermerInterface();
                 break;
             default:
+                logWarn(`Choix invalide saisi : "${choix}"`);
                 console.log(" Choix invalide.");
         }
     }
